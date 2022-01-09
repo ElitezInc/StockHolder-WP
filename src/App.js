6646f1a855a06ea92097e3a3f5eec9ecba98a909
ElitezInc <mistertank11@gmail.com>
2022-01-09 18:14:28 +0200

    Added products list component
    Added products list component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Navigation from "./components/Navigation";
+import ProductsList from "./components/ProductsList";
 
 function App() {
     return (
         <div className="App">
             <Navigation logoName={"logo.svg"}/>
+            <ProductsList />
         </div>
     );
 }
---
+++ src/components/ProductsList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Container} from "react-bootstrap";
+
+const ProductsList = ({
+  logoName
+}) => {
+
+  return (
+    <Container>
+        <p>Products list here</p>
+    </Container>
+  );
+};
+
+export default ProductsList;
