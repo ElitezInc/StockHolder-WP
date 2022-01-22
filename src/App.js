89589e643a0609cb92009f4649a2e23bd9fb0fcb
DevTalentBrain <holikzoda96@gmail.com>
2022-01-22 17:32:00 +0200

    Code restructurisation progress
    Code restructurisation progress

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Navigation from "./components/Navigation";
 import ProductsList from "./components/ProductsList";
-import Pagination from "./components/PaginationComponent";
-import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
-import { useEffect, useState } from "react";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-
-
-const api = new WooCommerceRestApi({
-    url: "http://localhost",
-    consumerKey: "ck_6c43d9b667acd786614f8faf427a243687a7a81e",
-    consumerSecret: "cs_d79dec45583d71e802af4067e9fefb410742bfec",
-    version: "wc/v3",
-  });
   
 function App() {
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-      fetchProducts();
-    }, []);
-  
-    let fetchProducts = () => {
-      api
-        .get("products", {
-          per_page: 20,
-        })
-        .then((response) => {
-          if (response.status === 200) {
-            setProducts(response.data);
-          }
-        })
-        .catch((error) => { console.error(error)});
-    };
-
     return (
         <div className="App">
             <Navigation logoName={"logo.svg"}/>
             <ProductsList data={products} />
-            <Pagination data={ProductsList}/>
         </div>
     );
 }
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -3,8 +3,41 @@ import { Container, Row, Col } from "react-bootstrap";
 import Card from 'react-bootstrap/Card'
 import Pagination from './PaginationComponent'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import WooCommerceRestApi from "@woocommerce/woocommerce-rest-api";
+import { useEffect, useState } from "react";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+const api = new WooCommerceRestApi({
+  url: "http://localhost",
+  consumerKey: "ck_6c43d9b667acd786614f8faf427a243687a7a81e",
+  consumerSecret: "cs_d79dec45583d71e802af4067e9fefb410742bfec",
+  version: "wc/v3",
+});
 
 class ProductsList extends Component {
+
+  const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+      fetchProducts();
+    }, []);
+  
+    let fetchProducts = () => {
+      api
+        .get("products", {
+          per_page: 20,
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            setProducts(response.data);
+          }
+        })
+        .catch((error) => { console.error(error)});
+    };
+
+  pageNavigation() {
+    console.log('on nav');
+  }
  
   renderCards = (products) => {
 
@@ -46,9 +79,10 @@ class ProductsList extends Component {
         { this.renderCards(this.props.data) }
   
         <Pagination
-          itemsCount={115}
+          itemsCount={145}
           itemsPerPage={15}
-          currentPage={1}
+          currentPage={5}
+          setCurrentPage={this.pageNavigation}
           alwaysShown={false}
         />
       </Container>
