c66b8e6273037e89edf9284e0352057c641afc4a
Gabrielius <mistertank11@gmail.com>
2022-03-06 14:40:31 +0200

    homepage changes
    homepage changes

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navigation from '../layout/Navigation';
 import { useEffect, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Card } from "react-bootstrap"
 import { useIsAuthenticated, useAuthUser } from 'react-auth-kit';
 
 const HomePage = () => {
@@ -13,12 +13,30 @@ const HomePage = () => {
       <Navigation logoName={"logo.svg"} />
       <Container>
         { isAuthenticated() ? auth().name : 'Not authenticated' }
-        <Row className="mt-4 mb-4">
-          <Col>
-            <div style={{height: '500px', backgroundColor: 'grey'}}></div>
-          </Col>
-        </Row>
       </Container>
+
+        <Container>
+          <Row className='gx-3'>
+            <Col lg={9}>
+              <Card.Body className='card-banner p-5 bg-primary' style={{height: "350px"}}>
+                <div style={{maxWidth: "500px"}}>
+                  <h1 className=''>Great products with <br/> best deals </h1>
+                  <p>No matter how far along you are in your sophistication as an amateur astronomer, there is always one.</p>
+                  <a href={window.location.href} className="btn btn-warning"> View more </a>
+                </div>
+              </Card.Body>
+            </Col>
+            <Col lg={3}>
+              <Card class="card-banner h-100" style={{backgroundColor: "var(--bs-warning)"}}>
+                <Card.Body class="card-body right text-center mt-4" style={{height: "250px"}}>
+                  <h5 class="mt-3 text-white">Amazing Gifts</h5>
+                  <p class="text-white">No matter how far along you are in your sophistication</p>
+                  <a href={window.location.href} className="btn btn-outline-light"> View more </a>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
     </>
   );
 }
